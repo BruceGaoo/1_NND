72cfb1302a041001a4c25039079f9bf1f978f743
BruceGao_Mac_1 <brucegao.1022@outlook.com>
2018-06-26 21:09:11 +0800

    2016.6.26

--- ShortCode/Code/result/Data_Processing.m
+++ ShortCode/Code/result/Data_Processing.m
@@ -1,5 +1,5 @@
-N = 16;
-K = 12;
+N = 64;
+K = 32;
 N = num2str(N);
 K = num2str(K);
 
@@ -21,11 +21,11 @@ ebn0_sc = ebn0';
 fer_sc = fer';
 ber_sc = ber';
 
-% % SCL L=2
-% load(['P_SCL2_', num2str(N), '_', num2str(K), '.mat']);
-% ebn0_scl2 = ebn0';
-% fer_scl2 = fer';
-% ber_scl2 = ber';
+% SCL L=2
+load(['P_SCL2_', num2str(N), '_', num2str(K), '.mat']);
+ebn0_scl2 = ebn0';
+fer_scl2 = fer';
+ber_scl2 = ber';
 
 % Processing
 ebn0 = ebn0_map(1:31);
@@ -33,8 +33,8 @@ fer_map = fer_map(1:31);
 ber_map = ber_map(1:31);
 fer_sc = fer_sc(1:31);
 ber_sc = ber_sc(1:31);
-% fer_scl2 = fer_scl2(1:31);
-% ber_scl2 = ber_scl2(1:31);
+fer_scl2 = fer_scl2(1:31);
+ber_scl2 = ber_scl2(1:31);
 
 
 % % Plot
@@ -56,5 +56,5 @@ ber_sc = ber_sc(1:31);
 %     'ebn0_nn', 'fer_nn', 'ber_nn', 'ebn0_map', 'fer_map', 'ber_map', ...
 %     'ebn0_sc', 'fer_sc', 'ber_sc', 'ebn0_scl2', 'fer_scl2', 'ber_scl2');
 
-save(['Compare_', num2str(N), '_', num2str(K), '.mat'], ...
-    'ebn0', 'ber_map', 'ber_nn', 'ber_sc');
+% save(['Compare_', num2str(N), '_', num2str(K), '.mat'], ...
+%     'ebn0', 'ber_map', 'ber_nn', 'ber_sc','ber_scl2');
